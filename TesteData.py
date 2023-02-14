153a012f784aded68ac0e10e7976887637403d4e
Pedro Siniscalchi <pedrosinisca@outlook.com.br>
2023-02-14 09:19:45 -0300

    Analysis

--- TesteData.py
+++ TesteData.py
@@ -1,1 +1,20 @@
-print("teste")
+import pandas as pd
+
+#Leitura do CSV
+dados = pd.read_csv("mnist_test.csv")
+
+#Show 5 Lines
+print(dados.head())
+
+#Descritiva
+print(dados.describe())
+
+#Filter
+filtro = dados['coluna']  == 'valor_especifico'
+dados_filtrados = dados[filtro]
+print(dados_filtrados)
+
+#Agrupa
+grupo = dados.groupby('coluna')
+media = grupo['outra_coluna'].mean()
+print(media)
